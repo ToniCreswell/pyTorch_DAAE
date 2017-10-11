8547a0381f88acb460d32b43f39489091f0678cc
ToniCreswell <ac2211@ic.ac.uk>
2017-10-11 16:29:06 +0100

    change default location of the data

--- scripts/DAAE.py
+++ scripts/DAAE.py
@@ -7,6 +7,7 @@ from dataload import CELEBA
 from models import DAE, DIS_Z
 
 import torch
+from torch import optim
 
 from torchvision.utils import make_grid, save_image
 from torchvision import transforms, datasets
