7d8c100e0a165b69bd28040e0ae0fa4546ec0f83
ToniCreswell <ac2211@ic.ac.uk>
2017-10-12 20:15:52 +0100

    add corruption to the sampling process

--- models.py
+++ models.py
@@ -101,6 +101,7 @@ class DAE(nn.Module):
 		save_image(x_i.data, join(exDir, 'samples0.png'))
 		for i in range(M):
 			z_i, x_i = self.forward(x_i)
+			x_i = self.corrupt(x_i)
 			save_image(x_i.data, join(exDir, 'samples'+str(i)+'.png'))
 
 class DIS_Z(nn.Module):
