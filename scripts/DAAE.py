04d60b24a96e189b0279c267d2390823c179b5c4
ToniCreswell <ac2211@ic.ac.uk>
2017-10-12 14:05:37 +0100

    fixing robustness plot

--- scripts/DAAE.py
+++ scripts/DAAE.py
@@ -84,7 +84,7 @@ def eval_mode(dae, exDir, M, testLoader):
 			xShift = shift_x(x, dy, dx)
 			encDxDy = dae.encode(x)
 			# diff = [(torch.dot(encDxDy[k], enc00[k])/ (torch.norm(encDxDy[k])*torch.norm(enc00[k]))).data[0] for k in range(encDxDy.size(0))]
-			diff = [(torch.dot(encDxDy[k], enc00[k])/ (torch.norm(encDxDy[k])*torch.norm(enc00[k]))),data for k in range(encDxDy.size(0))]
+			diff = [(torch.dot(encDxDy[k], enc00[k])/ (torch.norm(encDxDy[k])*torch.norm(enc00[k]))).data for k in range(encDxDy.size(0))]
 			robustnessMap[i,j] = np.mean(diff)
 			# robustnessMap[i,j] = np.mean(diff)
 
