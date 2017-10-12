2ea6a65857d65ac1fbe214d395a1202ca101550a
ToniCreswell <ac2211@ic.ac.uk>
2017-10-12 14:14:58 +0100

    debug rob plot

--- scripts/DAAE.py
+++ scripts/DAAE.py
@@ -78,18 +78,22 @@ def eval_mode(dae, exDir, M, testLoader):
 	robustnessMap = torch.Tensor(maxShift*2//step, maxShift*2//step).fill_(0)
 	x, y = prep_data(iter(testLoader).next(), useCUDA=dae.useCUDA)  #take a batch of samples
 	enc00 = dae.encode(x)
-	for j, dx in enumerate(axis):
-		for i, dy in enumerate(axis):
+	j=0
+	for dx in axis:
+		i=0
+		for dy in axis:
 			xShift = shift_x(x, dy, dx)
 			encDxDy = dae.encode(xShift)
 			# diff = [(torch.dot(encDxDy[k], enc00[k])/ (torch.norm(encDxDy[k])*torch.norm(enc00[k]))).data[0] for k in range(encDxDy.size(0))]
 			diff = [(torch.dot(encDxDy[k], enc00[k])/ (torch.norm(encDxDy[k])*torch.norm(enc00[k]))).data[0] for k in range(encDxDy.size(0))]
 			robustnessMap[i,j] = np.mean(diff)
-			# robustnessMap[i,j] = np.mean(diff)
+			i+=1
+		j+=1
+
 
 	fig1 = plt.figure()
 	print robustnessMap.min(), robustnessMap.max(), robustnessMap.size()
-	plt.imshow(robustnessMap.numpy()), extent=[-maxShift, maxShift, -maxShift, maxShift])
+	plt.imshow(robustnessMap.numpy(), extent=[-maxShift, maxShift, -maxShift, maxShift])
 	plt.colorbar()
 	plt.savefig(join(exDir, 'shiftRobustness.png'))
 
