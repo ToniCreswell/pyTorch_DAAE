6e8a768c7fb81096c2e31dafe0d628a2717dd1e4
ToniCreswell <ac2211@ic.ac.uk>
2017-11-24 10:26:02 +0000

    make a function for building dis

--- scripts/DAAE.py
+++ scripts/DAAE.py
@@ -49,7 +49,7 @@ def get_args():
 	parser.add_argument('--multimodalZ', action='store_true')
 
 	
-	return parser.parse_args(multimodalZ)
+	return parser.parse_args()
 
 def build_dis(dae, multimodalZ):
 	if not multimodalZ:
